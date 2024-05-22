305f87d5b13e593c9717b00c1f17d810fc689d28
acer 7-8 <acer@MacBook-Pro-acer.local>
2024-05-22 21:17:28 +0300

    Add new methods

--- methods.go
+++ methods.go
@@ -4,12 +4,27 @@ import (
 	"github.com/roma351/diary_api/resources"
 )
 
+func (api *DiaryAPI) Context(user *User, params map[string]interface{}) (res resources.Context, err error) {
+	err = api.UserRequest("/user/context", user, params, &res)
+	return
+}
+
+func (api *DiaryAPI) Account(user *User, params map[string]interface{}) (res interface{}, err error) {
+	err = api.UserRequest("/account/context", user, params, &res)
+	return
+}
+
 func (api *DiaryAPI) Last(user *User, params map[string]interface{}) (res resources.Last, err error) {
 	err = api.UserRequest("/school/last", user, params, &res)
 	return
 }
 
-func (api *DiaryAPI) Info() (res struct {
+func (api *DiaryAPI) Logout(user *User, params map[string]interface{}) (res resources.Last, err error) {
+	err = api.UserRequest("/account/logout", user, params, &res)
+	return
+}
+
+func (api *DiaryAPI) App() (res struct {
 	ID    int32   `json:"id"`
 	Title string  `json:"title"`
 	Image *string `json:"image"`
@@ -18,7 +33,7 @@ func (api *DiaryAPI) Info() (res struct {
 	return
 }
 
-func (api *DiaryAPI) InfoSecure(user *User) (res struct {
+func (api *DiaryAPI) AppSecure(user *User) (res struct {
 	UserId int64 `json:"user_id"`
 }, err error) {
 	err = api.UserRequest("/service/secure", user, nil, &res)
